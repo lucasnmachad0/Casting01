3fa055e15fe8399bca63aed81c357b2dc74f11d9
ALUNO <ALUNO@10.85.198.9>
2024-06-04 16:02:44 -0300

    Commit project Casting01

---
+++ src/Casting01.java
@@ -0,0 +1,15 @@
+public class Casting01 {
+    public static void main(String[] args) {
+        
+        byte valorByte = 127;
+        System.out.println("Valor inicial do byte: " + valorByte);
+        
+        
+        valorByte += 1;
+        System.out.println("Valor do byte apos adicionar 1: " + valorByte);
+        
+        
+        valorByte += 1;
+        System.out.println("Valor do byte apos adicionar novamente 1: " + valorByte);
+    }    
+}
